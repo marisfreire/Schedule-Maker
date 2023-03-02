4c9fb953bd722eb4025f6499024b106b9575b09f
Mariana Freire <121274971+marisfreire@users.noreply.github.com>
2023-03-01 21:01:01 -0300

    ver1.2.1
    Trouble with deployment

--- schedule-maker.js
+++ schedule-maker.js
@@ -2,86 +2,137 @@
 
 const scheduleEl = document.getElementById("schedule-el")
 const scheduleBtn = document.getElementById("schedule-btn")
+const inputEl = document.getElementById("input-el")
 
 let matéria = [ 
     
     {    nome: "Biologia 1",
-        assuntos: ["Ecologia"]
+        assuntos: ["Ecologia", "Proteínas", "Vitaminas"]
     } , 
     { 
         nome: "Biologia 2",
-        assuntos: ["Botânica"]
+        assuntos: ["Botânica", "Fisiologia Vegetal","Histologia Vegetal"]
     } ,
     { 
         nome: "Biologia 3",
-        assuntos: ["Biofísica "]
+        assuntos: ["Biofísica","Fisiologia Humana","Histologia Humana"]
     } ,
     { 
         nome: "Biologia 4",
-        assuntos: ["Genética"]
+        assuntos: ["Genética","Eutrofização","Zoologia"]
     } ,
     { 
         nome: "Matemática 1",
-        assuntos: ["Juros Simples"]
+        assuntos: ["Juros Simples","Juros Compostos","MDC e MMC"]
     } ,
     { 
         nome: "Matemática 2",
-        assuntos: ["Geometria Plana"]
+        assuntos: ["Geometria Plana","Geometria Espacial","Estatística"]
     } ,
     { 
         nome: "Matemática 3",
-        assuntos: ["Probabilidade"]
+        assuntos: ["Probabilidade","Análise Combinatória","Conjuntos"]
     } ,
     { 
         nome: "Matemática 4",
-        assuntos: ["Geometria analítica"]
+        assuntos: ["Geometria analítica","Retas","Círculo"]
+    } ,
+    { 
+        nome: "Matemática 5",
+        assuntos: ["Trigonometria no triângulo retângulo","Trigonometria básica","mais trigonometria"]
+    } ,
+    { 
+        nome: "História Geral",
+        assuntos: ["Era das Revoluções","Guerras Mundiais","Antiguidade Clássica"]
+    } ,
+    { 
+        nome: "História do Brasil",
+        assuntos: ["Colonização","Período Regencial","República"]
+    } ,
+    { 
+        nome: "Filosofia",
+        assuntos: ["Antiguidade","Kant","Nieztche"]
+    } ,
+    { 
+        nome: "Sociologia",
+        assuntos: ["Sociologia clássica","Sociologia brasileira","Sociologia contemporânea"]
+    } ,
+    { 
+        nome: "Interpretação",
+        assuntos: ["Tipologia textual","Texto e suas características","Figuras de Linguagem"]
+    } ,
+    { 
+        nome: "Literatura",
+        assuntos: ["Romantismo","Modernismo","Realismo"]
+    } ,
+    { 
+        nome: "Química 1",
+        assuntos: ["Ácidos e bases","Sais e óxidos","Atomística"]
+    } ,
+    { 
+        nome: "Química 2",
+        assuntos: ["Funções orgânicas","Compostos orgânicos","Isomeria"]
+    } ,
+    { 
+        nome: "Química 3",
+        assuntos: ["Tabela periódica","Ligações químicas","Radioatividade"]
+    } ,
+    { 
+        nome: "Química 4",
+        assuntos: ["Propriedades coligativas","Solubilização","Equilíbrio químico"]
+    } ,
+    { 
+        nome: "Física 1",
+        assuntos: ["Movimentos uniformes","Estática","Dinâmica"]
+    } ,
+    { 
+        nome: "Física 2",
+        assuntos: ["Eletrostática","Eletrodinâmica","Eletromagnetismo"]
+    } ,
+    { 
+        nome: "Física 3",
+        assuntos: ["","",""]
+    } ,
+    { 
+        nome: "Física 4",
+        assuntos: ["","",""]
+    } ,
+    { 
+        nome: "Inglês",
+        assuntos: ["","",""]
+    } ,
+    { 
+        nome: "Redação",
+        assuntos: ["","",""]
     } ,
 ]
 
 
-let j = Math.floor(Math.random() * 6)
 
-function gerarIndexRandom(){
+function gerarIndexRandomMateria(){
     let i = Math.floor(Math.random() * matéria.length)
+    return i
+}
+
+function gerarIndexRandomAssunto(){
+    let i = Math.floor(Math.random() * 3)
     return i
 }
 
 function gerar(){
-    gerarIndexRandom()
-    console.log(`${matéria[i].nome} : ${matéria[i].assuntos}`)
-
+    let index1 = gerarIndexRandomMateria()
+    let index2 = gerarIndexRandomAssunto()
+    let materia = matéria[index1].nome
+    let assunto = matéria[index1].assuntos[index2]
+    return `${materia}: ${assunto}`
 }
 
 
-
-
-
 scheduleBtn.addEventListener("click",function(){
-    gerar()
-})
-
-
-
-/* function generateRandomIndex(){
-    let i = Math.floor(Math.random() * matéria.length)
-    return i
-} */
-
-
-
-/* function generateSchedule(){
-    let materia1 = generateRandomIndex()
-    let materia2 = generateRandomIndex()
-    let materia3 = generateRandomIndex()
+    scheduleEl.innerHTML = ""
     
-    if ( materia1 != materia2 && materia1 != materia3 && materia2 != materia3){
-        scheduleEl.style.display = "block";
-        scheduleEl.textContent = `Hoje você estudará ${materia1}, ${materia2} e ${materia3}`
-        
-       
-    } else {
-        scheduleEl.textContent = "Erro, tente novamente!"
-        assuntosEl.textContent = ""
+    for (let i = 0; i < inputEl.value; i++){
+       let schedule = gerar()
+        scheduleEl.innerHTML += `<li>${schedule}</li>`
     }
-
-} */
+})
